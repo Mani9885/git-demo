0bdcbbd79853d6787916d8b12e3bb6de3d3d12e3
Mani9885 <manimaneesha9862@gmail.com>
2025-06-10 17:36:43 +0530

    added class

---
+++ src/main/java/com/example/demo/Constant.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Maneesha");
+
+	}
+
+}
